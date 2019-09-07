6a82f7bad9e14dc381117362618cbcd980d97dcd
Akshat Khare <cs1160315@cse.iitd.ac.in>
2019-09-08 02:41:48 +0530

    mlp done

--- instructions.h
+++ instructions.h
@@ -7,6 +7,28 @@
 #define LAB1C_INSTRUCTIONS_H
 #define loadInitialIndex "mov $0, %%rax\n\t"  \
                         "shl $3, %%rax\n\t" \
+                        "mov $5000, %%rsi\n\t"  \
+                        "shl $3, %%rsi\n\t" \
+                        "mov $6000, %%rdi\n\t"  \
+                        "shl $3, %%rdi\n\t" \
+                        "mov $7000, %%r8\n\t"  \
+                        "shl $3, %%r8\n\t" \
+                        "mov $8000, %%r9\n\t"  \
+                        "shl $3, %%r9\n\t" \
+                        "mov $9000, %%r10\n\t"  \
+                        "shl $3, %%r10\n\t" \
+                        "mov $4000, %%r11\n\t"  \
+                        "shl $3, %%r11\n\t" \
+                        "mov $3000, %%rbx\n\t"  \
+                        "shl $3, %%rbx\n\t" \
+                        "mov $2000, %%r12\n\t"  \
+                        "shl $3, %%r12\n\t" \
+                        "mov $1000, %%r13\n\t"  \
+                        "shl $3, %%r13\n\t" \
+                        "mov $1000000, %%r14\n\t"  \
+                        "shl $3, %%r14\n\t" \
+                        "mov $2000000, %%r15\n\t"  \
+                        "shl $3, %%r15\n\t" \
                         "mov $100000, %%rdx\n\t" \
                         "shl $3, %%rdx\n\t"
 #define loadArrayFromEntry1 "mov %2, %%rcx\n\t"
@@ -51,10 +73,22 @@
         nop10 \
         nop10
 #define movonlya "mov (%%rcx, %%rax,8), %%rax\n\t"
+#define movonlyrsi "mov (%%rcx, %%rsi, 8), %%rsi\n\t"
+#define movonlyrdi "mov (%%rcx, %%rdi, 8), %%rdi\n\t"
+#define movonlyr8 "mov (%%rcx, %%r8, 8), %%r8\n\t"
+#define movonlyr9 "mov (%%rcx, %%r9, 8), %%r9\n\t"
+#define movonlyr10 "mov (%%rcx, %%r10, 8), %%r10\n\t"
+#define movonlyr11 "mov (%%rcx, %%r11, 8), %%r11\n\t"
+#define movonlyrbx "mov (%%rcx, %%rbx, 8), %%rbx\n\t"
+#define movonlyr12 "mov (%%rcx, %%r12, 8), %%r12\n\t"
+#define movonlyr13 "mov (%%rcx, %%r13, 8), %%r13\n\t"
+#define movonlyr14 "mov (%%rcx, %%r14, 8), %%r14\n\t"
+#define movonlyr15 "mov (%%rcx, %%r15, 8), %%r15\n\t"
 #define movonlyd "mov (%%rcx, %%rdx,8), %%rdx\n\t"
 #define nopi \
-        nop100 nop100 nop10 nop10 nop1
-#define window1 movonlya nopi movonlyd nopi
+        nop100 
+// #define window1 movonlya movonlyrsi movonlyrdi movonlyr8 movonlyr9 movonlyr10 movonlyr11 movonlyrbx movonlyr12 movonlyr13 movonlyr14 movonlyr15 nopi movonlyd
+#define window1 movonlya movonlyrsi movonlyrdi movonlyr8 movonlyr9 movonlyr10 nopi movonlyd
 #define window10 \
         window1 \
         window1 \
